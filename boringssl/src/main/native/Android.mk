d54babe5d132d6945587efd52594834577c17563
vvb2060 <vvb2060@gmail.com>
2023-12-17 02:54:46 +0800

    Update ci

--- boringssl/src/main/native/Android.mk
+++ boringssl/src/main/native/Android.mk
@@ -7,7 +7,6 @@ LOCAL_MODULE            := crypto_test
 LOCAL_STATIC_LIBRARIES  := googletest_main crypto_static
 LOCAL_SRC_FILES         := $(crypto_test_sources)
 LOCAL_SRC_FILES         += $(test_support_sources)
-LOCAL_LDFLAGS           := -fPIE
 include $(LOCAL_PATH)/build-executable.mk
 
 include $(CLEAR_VARS)
@@ -15,14 +14,12 @@ LOCAL_MODULE            := ssl_test
 LOCAL_STATIC_LIBRARIES  := googletest_main ssl_static
 LOCAL_SRC_FILES         := $(ssl_test_sources)
 LOCAL_SRC_FILES         += $(test_support_sources)
-LOCAL_LDFLAGS           := -fPIE
 include $(LOCAL_PATH)/build-executable.mk
 
 include $(CLEAR_VARS)
 LOCAL_MODULE            := bssl
 LOCAL_STATIC_LIBRARIES  := crypto_static ssl_static
 LOCAL_SRC_FILES         := $(tool_sources)
-LOCAL_LDFLAGS           := -fPIE
 include $(LOCAL_PATH)/build-executable.mk
 
 include $(LOCAL_PATH)/BoringSSL.mk
